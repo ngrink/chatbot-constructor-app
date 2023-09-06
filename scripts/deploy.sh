779d12e5c31cb0c333674512f46bb0ebb103ada8
Nikolay Grinko <ngrink.io@gmail.com>
2023-09-06 23:53:10 +0300

    Add docker-compose.yaml for production

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,4 +5,4 @@ cd -
 rsync -rzvh -u -e 'ssh -p 2224' --progress --delete --exclude=node_modules ./services/web root@ngrink.ru:/srv/www/chatbot-constructor-app/services/
 rsync -rzvh -u -e 'ssh -p 2224' --progress --delete --exclude=node_modules ./services/server root@ngrink.ru:/srv/www/chatbot-constructor-app/services/
 rsync -rzvh -u -e 'ssh -p 2224' --progress --delete --exclude=node_modules ./services/core root@ngrink.ru:/srv/www/chatbot-constructor-app/services/
-rsync -rzvh -u -e 'ssh -p 2224' --progress --delete --exclude=node_modules ./docker-compose.yaml root@ngrink.ru:/srv/www/chatbot-constructor-app/
+rsync -rzvh -u -e 'ssh -p 2224' --progress --delete --exclude=node_modules ./docker-compose*.yaml root@ngrink.ru:/srv/www/chatbot-constructor-app/
